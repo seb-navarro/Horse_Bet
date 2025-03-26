2841af487465ea942a78f8c801333aa2de870110
seb-navarro <navarrosebastian154@gmail.com>
2025-03-26 12:33:21 +0000

    Added wait times between messages

--- Horse_bet.py
+++ Horse_bet.py
@@ -1,5 +1,5 @@
 import random
-
+from time import sleep
 
 wallet = 200
 
@@ -138,8 +138,10 @@ def bet():
 
         
     wallet -= stake
+    sleep(1)
     print("-----")
     print(f"You have placed a bet of £{stake} on {hbName}")
+    sleep(1)
     print(f"Your current balance is £{wallet}")
 
 
@@ -201,66 +203,107 @@ def resultLap():
     if Choc.finish and Moose.finish and Reggie.finish and Sol.finish:
         if tieTop:
             print(f"{lapR[0]} and {lapR[1]} finished the race joint at 1st place")
+            sleep(1)
             print(f"{lapR[2]} finished the race in 3rd place")
+            sleep(1)
             print(f"{lapR[3]} finished the race in last place")
+            sleep(1)
         elif tie3Top:
             print(f"{lapR[0]}, {lapR[1]}, and {lapR[2]} finished the race joint at 1st place")
+            sleep(1)
             print(f"{lapR[3]} finished the race in last place")
+            sleep(1)
         elif tieFull:
             print(f"All horses finished the race tied")
+            sleep(1)
         elif tieLast:
             print(f"{lapR[0]} finished the race in 1st place")
+            sleep(1)
             print(f"{lapR[1]} finished the race in 2nd place")
+            sleep(1)
             print(f"{lapR[2]} and {lapR[3]} finished the race joint at 3rd place")
+            sleep(1)
         elif tie3Last:
             print(f"{lapR[0]} finished the race in 1st place")
+            sleep(1)
             print(f"{lapR[1]}, {lapR[2]}, and {lapR[3]} finished the race joint at 2nd place")
+            sleep(1)
         elif tieSecond:
             print(f"{lapR[0]} finished the race in 1st place")
+            sleep(1)
             print(f"{lapR[1]} and {lapR[2]} finished the race joint at 2nd place")
+            sleep(1)
             print(f"{lapR[3]} finished the race in last place")
+            sleep(1)
         elif tieDouble:
             print(f"{lapR[0]} and {lapR[1]} finished the race joint at 1st place")
+            sleep(1)
             print(f"{lapR[2]} and {lapR[3]} finished the race joint at 2nd place")
+            sleep(1)
         else:
             print(f"{lapR[0]} finished the race in 1st place")
+            sleep(1)
             print(f"{lapR[1]} finished the race in 2nd place")
+            sleep(1)
             print(f"{lapR[2]} finished the race in 3rd place")
+            sleep(1)
             print(f"{lapR[3]} finished the race in last place")
+            sleep(1)
 
     if not Choc.finish or not Moose.finish or not Reggie.finish or not Sol.finish:
         if tieTop:
             print(f"{lapR[0]} and {lapR[1]} are currently joint at 1st place")
+            sleep(1)
             print(f"{lapR[2]} is currently in 3rd place")
+            sleep(1)
             print(f"{lapR[3]} is currently in last place")
+            sleep(1)
         elif tie3Top:
             print(f"{lapR[0]}, {lapR[1]}, and {lapR[2]} are currently joint at 1st place")
+            sleep(1)
             print(f"{lapR[3]} is currently in last place")
+            sleep(1)
         elif tieFull:
             print(f"All horses are currently tied")
+            sleep(1)
         elif tieLast:
             print(f"{lapR[0]} is currently in 1st place")
+            sleep(1)
             print(f"{lapR[1]} is currently in 2nd place")
+            sleep(1)
             print(f"{lapR[2]} and {lapR[3]} are currently joint at 3rd place")
+            sleep(1)
         elif tie3Last:
             print(f"{lapR[0]} is currently in 1st place")
+            sleep(1)
             print(f"{lapR[1]}, {lapR[2]}, and {lapR[3]} are currently joint at 2nd place")
+            sleep(1)
         elif tieSecond:
             print(f"{lapR[0]} is currently in 1st place")
+            sleep(1)
             print(f"{lapR[1]} and {lapR[2]} are currently joint at 2nd place")
+            sleep(1)
             print(f"{lapR[3]} is currently in last place")
+            sleep(1)
         elif tieDouble:
             print(f"{lapR[0]} and {lapR[1]} are currently joint at 1st place")
+            sleep(1)
             print(f"{lapR[2]} and {lapR[3]} are currently joint at 2nd place")
+            sleep(1)
         else:
             print(f"{lapR[0]} is currently in 1st place")
+            sleep(1)
             print(f"{lapR[1]} is currently in 2nd place")
+            sleep(1)
             print(f"{lapR[2]} is currently in 3rd place")
+            sleep(1)
             print(f"{lapR[3]} is currently in last place")
+            sleep(1)
 
         print("-----")
         print("Press enter to continue")
         input("-----")
+        sleep(1)
 
 
 def resultBet():
@@ -410,6 +453,7 @@ def resultBet():
 
 def runRace():
     print(f"Your balance is £{wallet}")
+    sleep(1)
 
     bet()
 
@@ -418,74 +462,110 @@ def runRace():
     input("-----")
 
     print("BANG!")
+    sleep(1)
     print("The horses are off!")
+    sleep(1)
 
     print("-----")
     print("Press enter to continue")
     input("-----")
+    sleep(1)
 
     print("LAP 1")
     print(" ")
+    sleep(1)
     Choc.runL1()
+    sleep(1)
     Moose.runL1()
+    sleep(1)
     Reggie.runL1()
+    sleep(1)
     Sol.runL1()
+    sleep(1)
 
     resultLap()
 
     print("LAP 2")
     print(" ")
+    sleep(1)
     Choc.runL2()
+    sleep(1)
     Moose.runL2()
+    sleep(1)
     Reggie.runL2()
+    sleep(1)
     Sol.runL2()
+    sleep(1)
 
     resultLap()
 
     print("LAP 3")
     print(" ")
+    sleep(1)
     Choc.runL3()
+    sleep(1)
     Moose.runL3()
+    sleep(1)
     Reggie.runL3()
+    sleep(1)
     Sol.runL3()
+    sleep(1)
 
     resultLap()
 
     print("LAP 4")
     print(" ")
+    sleep(1)
     Choc.runL4()
+    sleep(1)
     Moose.runL4()
+    sleep(1)
     Reggie.runL4()
+    sleep(1)
     Sol.runL4()
+    sleep(1)
 
     resultLap()
 
     print("LAP 5")
     print(" ")
+    sleep(1)
     Choc.runL5()
+    sleep(1)
     Moose.runL5()
+    sleep(1)
     Reggie.runL5()
+    sleep(1)
     Sol.runL5()
+    sleep(1)
 
     resultLap()
 
     print("FINAL LAP")
     print(" ")
+    sleep(1)
     Choc.runL6()
+    sleep(1)
     Moose.runL6()
+    sleep(1)
     Reggie.runL6()
+    sleep(1)
     Sol.runL6()
+    sleep(1)
 
     resultLap()
 
     print("-----")
     print("Press enter to continue")
     input("-----")
+    sleep(1)
 
     print("BET RESULTS")
     print(" ")
+    sleep(1)
     resultBet()
     print ("-----")
+    sleep(1)
 
     Choc.finish = False
     Moose.finish = False
@@ -505,27 +585,34 @@ def runRace():
                 break
 
         if decision == "N" or decision == "n":
+            sleep(1)
             print(" ")
             print("********************")
             print(f"Congratulations! You have walked away with £{wallet}.")
             print("********************")
             print(" ")
+            sleep(1)
             print("Thank you for visiting the racecourse. Goodbye!")
             input(" ")
         elif decision == "Y" or decision == "y":
+            sleep(1)
             print(" ")
             print("********************")
             print("Welcome Back to Race Day")
             print("********************")
             print(" ")
+            sleep(1)
             runRace()
                 
 
     else:
+        sleep(1)
         print("You have gambled all your money away and cannot continue.")
         print(" ")
+        sleep(1)
         print("Thank you for visiting the racecourse. Goodbye!")
         print(" ")
+        sleep(1)
 
 
 print(" ")
@@ -538,14 +625,22 @@ print("Press enter to see the rules")
 input("-----")
 
 print("RACECOURSE RULES")
+sleep(1)
 print(" ")
 print("1. All horses have 4/1 odds - Example: £10 bet on the winning horse will return £50 (£40 winnings + £10 stake).")
+sleep(1)
 print ("2. We only accept bets to the nearest pound.")
+sleep(1)
 print ("3. If your returns include pennies then the final value will be rounded to the nearest pound.")
+sleep(1)
 print("4. If the race finishes in a draw between your horse and another horse, you will recieve half of the expected winnings plus your stake.")
+sleep(1)
 print("5. If the race finishes in a draw between your horse and two other horses you will recieve a third of the expected winnings plus your stake.")
+sleep(1)
 print("6. If all horses finish the race in a draw then you will only recieve your stake back.")
+sleep(1)
 print("7. One race runs for six laps.")
+sleep(1)
 
 print("-----")
 print("Press enter to acknowledge the rules")
